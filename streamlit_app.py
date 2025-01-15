5644b9cb50a00694f13501ad75bce2655cc50b6d
emrebaser0 <emrebaser.official@gmail.com>
2025-01-15 23:29:29 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+def kod(data):
+    st.write(data*2)
+
+data = st.number_input("sayı gir:")
+result = kod(data)
+st.write(f"Sonuç: {result}")
